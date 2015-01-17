d16199dfc7113725fa12d9eeb40580b4e286513d
Thanathep Thaithae <supatpolsutan@supats-iMac.local>
2015-01-17 14:04:56 +0700

    Try Boolean

--- Swift101.playground/section-1.swift
+++ Swift101.playground/section-1.swift
@@ -1,7 +1,15 @@
-// Playground - noun: a place where people can play
-
 import UIKit
 
 var str = "Hello, playground"
 
+
 println(str)
+
+let myAge=20
+
+var suanStudent:Bool = true
+
+//content
+println("I am \(myAge) years old")
+println("Am I Suankularb Student : \(suanStudent)")
+
