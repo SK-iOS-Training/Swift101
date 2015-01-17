e081ba189e4208c658724d8d6790f849627a5463
Waitaya Krongapiradee <wyte.sk133@gmail.com>
2015-01-17 14:19:35 +0700

    Lab 3.2

---
+++ Lab32PG.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+func printA ()->Void {
+    println("   *   ")
+    println("  ***  ")
+    println(" ***** ")
+    println("*******")
+    println("   *   ")
+    println("   *   ")
+    println("   *   ")
+}
+
+printA()
+printA()
