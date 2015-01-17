ab285be6fe9092e7e188359be22a2b3f8430f819
Waitaya Krongapiradee <wyte.sk133@gmail.com>
2015-01-17 16:32:54 +0700

    Lab 4.x

---
+++ Lab42PG.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+func oddInt(maxN:Int) {
+    var vOod:Int = 1
+    while(vOod<=maxN)
+    {
+        println(" \(vOod)")
+        vOod = vOod + 2
+    }
+}
+
+oddInt(8)
+oddInt(10)
