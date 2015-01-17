b57d91311323277a34edaad8ac25d67f5e044e65
Waitaya Krongapiradee <wyte.sk133@gmail.com>
2015-01-17 15:36:55 +0700

    Lab 3.3

---
+++ Lab33PG.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+func sayGreeting (greeting:String, name:String)->Void {
+    println("*** Swift Programming ***")
+    println("*** \(greeting) \(name) ***")
+}
+
+sayGreeting("Hello", "Lek")
+
+sayGreeting("Sawasdee", "Ton")
